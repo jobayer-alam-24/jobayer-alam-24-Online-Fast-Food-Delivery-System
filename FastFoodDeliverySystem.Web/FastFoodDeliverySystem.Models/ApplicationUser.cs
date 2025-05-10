6c70ec82328f445eb1daf97680fc8790d2237e3e
jobayer-alam-24 <alamsheikhjobayer2000@gmail.com>
2025-05-10 22:46:45 +0600

    working

---
+++ FastFoodDeliverySystem.Web/FastFoodDeliverySystem.Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace FastFoodDeliverySystem.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        [DisplayName("Postal Code")]
+        public string PostalCode { get; set; }
+    }
+}
---
+++ FastFoodDeliverySystem.Web/FastFoodDeliverySystem.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFoodDeliverySystem.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is Required.")]
+        public string Title { get; set; }
+      
+    }
+}
